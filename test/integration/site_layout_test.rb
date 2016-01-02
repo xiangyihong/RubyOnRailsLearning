5cf15c3caa255609f8290004ccc615efeeb2be61
Xiang Yihong <xiangyihong@gmail.com>
2016-01-02 19:30:32 +0800

    Stop using variable name as the first argument of assert_select It is a trap that the content of the variable will be modified by assert_select Use a raw string as the first argument

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,12 +6,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   # end
 
   test "layout links" do
-    selecter = "a[href=?]"
     get root_path
     assert_template 'static_pages/home'
-    assert_select selecter, home_path, count: 2
-    # assert_select selecter, help_path
-    assert_select selecter, about_path
-    # assert_select selecter, contact_path
+    assert_select "a[href=?]", home_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
   end
 end
