a895a1a57f8240824ee916d7b97a2271788833a6
Xiang Yihong <xiangyihong@gmail.com>
2016-01-02 21:31:29 +0800

    Extract basic page test functionality to one method

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,22 +2,27 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
-    get :home
-    assert_response :success
+    basic_page_test(:home)
   end
 
   test "should get help" do
-    get :help
-    assert_response :success
+    basic_page_test(:help)
   end
 
   test "should get about" do
-    get :about
-    assert_response :success
+    basic_page_test(:about)
   end
 
   test "should get contact" do
-    get :contact
-    assert_response :success
+    basic_page_test(:contact)
   end
+
+  def basic_page_test(page)
+    # page should be a symbol
+    raise TypeError.new('Should pass Symbol') unless page.is_a? Symbol
+    get page
+    assert_response :success
+    assert_select 'title', "#{page.to_s.capitalize} | Ruby on Rails Learning"
+  end
+
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,12 +5,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
-  test "layout links" do
+  test 'layout links' do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", home_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
+    assert_select 'a[href=?]', home_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', signup_path
   end
 end
