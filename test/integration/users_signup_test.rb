0e6fc1c10ada2c56b97ef3e3e241c4c2521bf52d
Xiang, Yihong <xiangyihong@gmail.com>
2016-01-07 01:14:24 +0000

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -4,4 +4,25 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: {name: "",
+                              email: "user@invalid",
+                              password: "foo",
+                              password_confirmation: "bar"}
+    end
+    assert_template 'users/new'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: {name: "Test User",
+                                          email: "TestUser@test.com",
+                                          password: "password",
+                                          password_confirmation: "password"}
+    end
+    assert_template 'users/show'
+  end
 end
