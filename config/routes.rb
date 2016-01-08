81e6f53fee39bdd80d4c9bc832ecaba04d077fc9
Xiang, Yihong <xiangyihong@gmail.com>
2016-01-08 23:21:15 +0000

    start to add session

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   get 'home' => 'static_pages#home'
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'session#create'
+  get 'logout' => 'session#destroy'
   #
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
